ee8b769bfafa4848617ffebf4f3f79a6041244b7
Sudip Sikdar <sudip.sikdar@gmail.com>
2020-09-05 19:57:15 +0800

    Update counter.jsx
    event handler added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,25 +3,36 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
-    tags: [],
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
 
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
+  handleIncrement = () => {
+    console.log("Increment clicked", this.state.count);
+  };
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag !"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
